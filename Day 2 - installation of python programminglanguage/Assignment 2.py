f234620536e975b655686e2cc2171fc72ff2b2dc
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2023-05-15 01:04:53 +0530

    Add files via upload

---
+++ Day 2 - installation of python programminglanguage/Assignment 2.py
@@ -0,0 +1,27 @@
+"""for i in range(2000,2101):
+    print(i)
+    
+    if (i%7==0):
+        print("The number which is in given range is divisible by 7")
+        if (i%5!=0):
+            print("number is not divisible by 5")
+        else:
+            print("Numer is the multiple of 5 also along with 7")
+    else:
+        print("number is not a multiple of 7so case rises")
+"""
+for i in range(2000,3001):
+    print(i)
+    if (i%7==0 and i%5!=0):
+        print("So this no. is divisible by 7 while  not divisible by 5")
+
+        
+            
+
+
+
+      
+        
+            
+
+        
---
+++ Day 2 - installation of python programminglanguage/Basic Commands in python for Ai course.py
@@ -0,0 +1,340 @@
+#basic Operationsin Pythn
+"""a = 45
+b = 6.5
+c = a + b
+print(c)#51.5
+
+a = input("Enter the value of a : ")
+b = input("Agian put the value of b : ")
+c = int(a) + int(b)
+#print("Addition of two numbers {0} and {1} is {2}".format(a,b,c))
+#Enter the value of a : 45
+#Agian put the value of b : 10
+#Addition of two numbers 45 and 10 is 55
+"""
+#Taking input from user and understanding of Looping as well as Nested Loop statements
+"""
+a = input("Enter the value of a : ")
+b = input("Agian put the value of b : ")
+c = input("enter the value of c: ")
+if (a>b):
+    print("a is greater than b")
+    if (c>a):
+        print("c is greatest")
+        else:
+            print("c is smallest")
+
+            elif(a==b):
+                print("a is equal to b")
+
+
+                else:
+                    print("a is lesser than b")
+                    """
+#Understanding of For as well as While Loops
+"""
+i = 100
+while(i>2):
+    print(i)
+    i-=1
+
+
+for i in range(10):
+    print(i)
+    
+for j in range(10,50,2):
+    print(j)
+"""
+
+#Function Definition and Function Calling
+"""
+def addition (a,b):
+    c = a + b;
+    return c
+
+a = 10
+b = 10
+ans = addition(a,b)
+print("Ans is {0}".format(ans)) #Ans is 20
+"""
+#File handling in Python Progeaqmming language
+"""
+a = open("Shubham.txt","r")
+print(a.read())
+a.close
+#Hey, Now a days i am bussy in learning of Artificial intelligence
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                      
+                
+        
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
